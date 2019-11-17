703df46347782d3791e5c74b12589bb873aeae11
Kyle <kylepcole@gmail.com>
2019-11-17 16:52:44 -0500

    finallay passing

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,25 @@
 class Item
+
   attr_accessor :name, :price
+
+  # @@all = []
 
   def initialize(name,price)
     @name = name
     @price = price
+    # @@all << self
   end
+
+  # def self.all
+  # 	@@all
+  # end
+
+  # def self.find(string)
+  # 	Item.all.find do |item| 
+  # 		item.name.downcase == string.downcase
+  # 	end
+  # end
+
+  
+
 end
